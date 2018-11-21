1d8b4d045030315e11c7e640597c35f13fcb7d15
bobae7 <bobae7@me.com>
2018-11-21 23:54:02 +0900

    추가

--- Chapter05/Chapter05.cpp
+++ Chapter05/Chapter05.cpp
@@ -3,6 +3,9 @@
 #include <deque>
 #include <list>
 #include <algorithm>
+#include <stack>
+#include <functional>
+#include <set>
 
 using namespace std;
 
@@ -168,11 +171,230 @@ namespace S5_6
     }
 }
 #pragma endregion Sample 5-6
+#pragma region S5_7
+namespace S5_7
+{
+    int main(void)
+    {
+        vector<int> v;
+        v.push_back(50);
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(40);
+        v.push_back(30);
+
+        sort(v.begin(), v.end(), less<int>());
+        for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+            cout << *iter << " ";
+        cout << endl;
+
+        sort(v.begin(), v.end(), greater<int>());
+        for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+            cout << *iter << " ";
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-7
+#pragma region S5_8
+namespace S5_8
+{
+    int main(void)
+    {
+        stack<int> st;                  // stack 컨테이너 생성
+
+        st.push(10);                    // 데이터 추가(입력)
+        st.push(20);
+        st.push(30);
+
+        cout << st.top() << endl;       // top 데이터 출력
+        st.pop();                       // top 데이터 삭제
+        cout << st.top() << endl;
+        st.pop();
+        cout << st.top() << endl;
+        st.pop();
+
+        if (st.empty())
+            cout << "stack에 데이터 없음" << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-8
+#pragma region S5_9
+namespace S5_9
+{
+    int main(void)
+    {
+        stack<int, vector<int>> st;
+
+        st.push(10);                    // 데이터 추가(입력)
+        st.push(20);
+        st.push(30);
+
+        cout << st.top() << endl;       // top 데이터 출력
+        st.pop();                       // top 데이터 삭제
+        cout << st.top() << endl;
+        st.pop();
+        cout << st.top() << endl;
+        st.pop();
+
+        if (st.empty())
+            cout << "stack에 데이터 없음" << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-9
+#pragma region S5_10
+namespace S5_10
+{
+    int main(void)
+    {
+        vector<int> v;
+
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+        v.push_back(40);
+        v.push_back(50);
+
+        for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+            cout << *iter << " ";
+        cout << endl;
+
+        // 일반 반복자 iterator를 역방향 반복자 reverse_iterator로 변환
+        reverse_iterator<vector<int>::iterator> riter(v.end());
+        reverse_iterator<vector<int>::iterator> end_riter(v.begin());
+
+        for (; riter != end_riter; ++riter)
+            cout << *riter << " ";
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-10
+#pragma region S5_11
+namespace S5_11
+{
+    int main(void)
+    {
+        vector<int> v;
+
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+        v.push_back(40);
+        v.push_back(50);
+
+        for (vector<int>::iterator iter = v.begin(); iter != v.end(); ++iter)
+            cout << *iter << " ";
+        cout << endl;
+
+        // STL 모든 컨테이너는 반복자 어댑터 reverse_iterator를 typedef 타입으로 정의하며
+        // rbegin(), rend()로 컨테이너의 역방향 반복자를 반환
+        vector<int>::reverse_iterator riter(v.rbegin());
+        for (; riter != v.rend(); ++riter)
+            cout << *riter << " ";
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-11
+#pragma region S5_12
+namespace S5_12
+{
+    int main(void)
+    {
+        vector<int> v;
+
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+        v.push_back(40);
+        v.push_back(50);
+
+        vector<int>::iterator normal_iter = v.begin() + 3;          // 40 원소를 가리키는 정방향 반복자
+        vector<int>::reverse_iterator reverse_iter(normal_iter);    // normal_iter의 역방향 반복자
+
+        cout << "정방향 반복자의 값: " << *normal_iter << endl;
+        cout << "역방향 반복자의 값: " << *reverse_iter << endl;
+        cout << endl;
+
+        for (vector<int>::iterator iter = v.begin(); iter != normal_iter; ++iter)
+            cout << *iter << " ";       // 정방향 출력
+        cout << endl;
+
+        for (vector<int>::reverse_iterator riter = reverse_iter; riter != v.rend(); ++riter)
+            cout << *riter << " ";
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-12
+#pragma region S5_13
+namespace S5_13
+{
+    int main(void)
+    {
+        cout << less<int>()(10, 20) << endl;            // 임시 less 객체로 비교
+        cout << less<int>()(20, 20) << endl;
+        cout << less<int>()(20, 10) << endl;
+        cout << "==============" << endl;
+        cout << not2(less<int>())(10, 20) << endl;      // 임시 less 객체에 not2 어댑터 적용
+        cout << not2(less<int>())(20, 20) << endl;
+        cout << not2(less<int>())(20, 10) << endl;
+        cout << endl;
+
+        less<int> l;
+        cout << l(10, 20) << endl;      // less 객체 l로 비교
+        cout << l(20, 20) << endl;
+        cout << l(20, 10) << endl;
+        cout << "==============" << endl;
+        cout << not2(l)(10, 20) << endl;
+        cout << not2(l)(20, 20) << endl;
+        cout << not2(l)(20, 10) << endl;
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-13
+#pragma region S5_14
+namespace S5_14
+{
+    int main(void)
+    {
+        // vector<typename T, typename Alloc = allocator<T>>, vector<int>와 같음
+        vector<int, allocator<int>> v;
+        v.push_back(10);
+        cout << *v.begin() << endl;
+
+        // set<typename T, typename Pred = less<T>, typename Alooc = allocator<T>>, set<int>와 같음
+        set<int, less<int>, allocator<int>> s;
+        s.insert(10);
+        cout << *s.begin() << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 5-14
 
 int main(void)
 {
-    S5_6::main();
-    //E5_1::main();
+    S5_14::main();
 }
 
 /*
---
+++ Chapter06/Chapter06.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#pragma region S6_1
+namespace S6_1
+{
+
+
+    int main(void)
+    {
+
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 6-1
+
+
+int main(void)
+{
+    S6_1::main();
+}
+
+/*
+#pragma region S6_
+namespace S6_
+{
+
+
+    int main(void)
+    {
+
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 6-
+*/

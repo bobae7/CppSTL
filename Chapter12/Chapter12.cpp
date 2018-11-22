51abefb6a6a4ababf415dd7a0e0cf69210d1c2f7
bobae7 <bobae7@me.com>
2018-11-23 07:26:43 +0900

    12

--- Chapter12/Chapter12.cpp
+++ Chapter12/Chapter12.cpp
@@ -119,11 +119,159 @@ namespace S12_3
     }
 }
 #pragma endregion Sample 12-3
+#pragma region S12_4
+namespace S12_4
+{
+    int main(void)
+    {
+        string s("Hello!");
+        const char *sz;
+        const char *buf;
+
+        sz = s.c_str();
+        buf = s.data();
+
+        cout << "'\\0'문자로 끝나는 문자열(C-Style): " << sz << endl;
+        cout << "'\\0'문자 포함하지 않는 문자열 배열: " << buf << endl;
+        for (int i = 0; i < 6; ++i)
+            cout << buf[i];
+        cout << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 12-4
+#pragma region S12_5
+namespace S12_5
+{
+    int main(void)
+    {
+        string s1("ABCDE");
+        string s2("AKABC");
+        const char* sz = "AKABC";
+
+        // s: string 객체, sz: '\0' 문자열, off: 시작위치, n: 길이
+        cout << "s1.compare(s): " << s1.compare(s2) << endl;
+        cout << "s1.compare(off, n, s): " << s1.compare(2, 3, s2) << endl;
+        cout << "s1.compare(off, n, s, off2, n2): " << s1.compare(0, 3, s2, 2, 3) << endl;
+        cout << "s1.compare(sz): " << s1.compare(sz) << endl;
+        cout << "s1.compare(off, n, sz): " << s1.compare(2, 3, sz) << endl;
+        cout << "s1.compare(off, n, sz, n2): " << s1.compare(0, 1, sz, 1) << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 12-5
+#pragma region S12_6
+namespace S12_6
+{
+    int main(void)
+    {
+        string s("Hello!");
+        char buf[100];
+
+        // 주의점: copy()는 끝에 '\0' 문자를 포함하지 않는다.
+        s.copy(buf, s.length());    // length()는 size()와 같다.
+        buf[s.length()] = '\0';
+        cout << "전체 문자열[copy(buf, n)]: " << buf << endl;
+        s.copy(buf, 4, 2);
+        buf[4] = '\0';
+        cout << "부분 문자열[copy(buf, n, off)]: " << buf << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 12-6
+#pragma region S12_7
+namespace S12_7
+{
+    int main(void)
+    {
+        const char *sz = "\"Be careful in Uncle Randy's new car\", "
+            "my sister told them. \"Wipe your feet before you get in it. "
+            "Don't mess anything up. Don't get it dirty.\" "
+            "I listened to her, and thought, "
+            "as only a bachelor uncle can So I made things easy. "
+            "while my sister was outlining the rules, "
+            "I slowly and deliberately opened a can of soda, turned it over, "
+            "and poured it on the cloth seats in the back of the convertible.";
+        string t("Randy");
+        string s = sz;
+
+        //s:string객체, sz:'\0'문자열, c:문자, off:시작위치, n:길이
+        cout << s << endl << endl;
+        cout << "s.find(c): " << s.find('I') << endl;
+        cout << "s.find(c,off): " << s.find('I', 250) << endl;
+        cout << "s.find(sz): " << s.find("poured it") << endl;
+        cout << "s.find(sz,off): " << s.find("poured it", 0) << endl;
+        cout << "s.find(sz,off,n): " << s.find("I listened to her", 0, 1) << endl;
+        cout << "s.find(c): " << s.find(t, 0) << endl << endl;
+
+        string::size_type pos = s.find("Not found");
+        cout << (int)pos << " : " << (int)string::npos << endl;
+
+        pos = s.rfind('I');
+        if (string::npos != pos)
+            cout << "s.rfind(c): " << pos << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 12-7
+#pragma region S12_8
+namespace S12_8
+{
+    int main(void)
+    {
+        string t("ABC");
+
+        string s1("Hello!");
+        string s2("Hello!");
+        string s3("Hello!");
+        string s4("Hello!");
+        string s5("Hello!");
+        string s6("Hello!");
+        string s7("Hello!");
+        string s8("Hello!");
+        string s9("Hello!");
+
+        s1.insert(1, "ABC");
+        s2.insert(1, "ABC", 2);
+        s3.insert(1, t);
+        s4.insert(1, t, 0, 2);
+        s5.insert(1, 3, 'A');
+
+        s6.insert(s6.begin() + 1, ' ');
+        s7.insert(s7.begin() + 1, 'A');
+        s8.insert(s8.begin() + 1, 3, 'A');
+        s9.insert(s9.begin() + 1, t.begin(), t.end());
+
+        //s:string객체,sz:'\0'문자열,c:문자,pos:삽입위치,positer:삽입반복자,off:위치,n:길이
+        cout << "s1.insert(pos, sz): " << s1 << endl;
+        cout << "s2.insert(pos, sz, n): " << s2 << endl;
+        cout << "s3.insert(pos, s): " << s3 << endl;
+        cout << "s4.insert(pos, s, off, n): " << s4 << endl;
+        cout << "s5.insert(pos, n, c): " << s5 << endl;
+
+        cout << "s6.insert(positer): " << s6 << endl;
+        cout << "s7.insert(positer, c): " << s7 << endl;
+        cout << "s8.insert(positer, n, c): " << s8 << endl;
+        cout << "s9.insert(positer, iter1, iter2): " << s9 << endl;
+
+        system("pause");
+        return 0;
+    }
+}
+#pragma endregion Sample 12-8
 
 
 int main(void)
 {
-    S12_3::main();
+    S12_8::main();
 }
 
 /*
